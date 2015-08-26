e9db36cd0d889c9c9a7ac5d7a63b797b104b123a
Loric Brevet <loric.brevet@gmail.com>
2015-08-26 22:58:00 +0200

    Set flat theme

--- bcvscore/settings.py
+++ bcvscore/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
-    'suit',
+    'flat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
