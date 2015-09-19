9f869e27e63e96693ec672274e12091dccf1047e
Loric Brevet <loric.brevet@gmail.com>
2015-09-19 15:00:42 +0200

    Split templates and add search functionnality

--- score/urls.py
+++ score/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'score.views',
     url(r'^$', 'home'),
+    url(r'^upcoming', 'upcoming')
 )
--- score/views.py
+++ score/views.py
@@ -1,12 +1,54 @@
 from django.shortcuts import render
 from score.models import Rencontre
+from django.db.models import Q
+import datetime
 
 
 def home(request):
-    rencontres = Rencontre.objects.all().order_by('-date')
+    home = True
+    title = "Résultats"
+
+    today = datetime.date.today()
+    next_sunday = today + datetime.timedelta(6-today.weekday())
+
+    rencontres = Rencontre.objects.filter(
+        date__lte=next_sunday
+    ).order_by(
+        '-date',
+        '-heure'
+    )
+
+    if 'q' in request.GET:
+        q = request.GET['q']
+        query = Q(equipeDom__nom__icontains=q) | Q(equipeExt__nom__icontains=q)
+        rencontres = rencontres.filter(query)
+
     return render(
         request,
-        'score/home.html',
+        'score/results.html',
+        locals()
+    )
+
+
+def upcoming(request):
+    home = False
+    title = "Matchs à venir"
+
+    rencontres = Rencontre.objects.filter(
+        date__gte=datetime.date.today()
+    ).order_by(
+        'date',
+        'heure'
+    )
+
+    if 'q' in request.GET:
+        q = request.GET['q']
+        query = Q(equipeDom__nom__icontains=q) | Q(equipeExt__nom__icontains=q)
+        rencontres = rencontres.filter(query)
+
+    return render(
+        request,
+        'score/results.html',
         locals()
     )
 
